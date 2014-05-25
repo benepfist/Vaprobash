f9135da850c1857f6b9dc0dbe0b442b09bd47c50
Benedict Pfisterer <pfisterer.bene@gmail.com>
2014-05-25 22:36:07 +0200

    short the script
    short the script

--- scripts/gulpjs.sh
+++ scripts/gulpjs.sh
@@ -5,20 +5,15 @@ node -v > /dev/null 2>&1
 NODE_IS_INSTALLED=$?
 
 # True, if Node is not installed
-if [[ $NODE_IS_INSTALLED -ne 0 ]]; then
+if [ $NODE_IS_INSTALLED -ne 0 ] 
+then
 	echo ">>>> First install Node"
 	exit
 fi
 
 echo ">>>> Install gulp and plugins"
 
-# Default gulp plugins
-default=( gulp gulp-ruby-sass gulp-util gulp-autoprefixer gulp-minify-css gulp-jsmin gulp-concat gulp-rename )
-
-# Merge with parameters
-plugins=("$@" "${default[@]}")
-
-npm install "${plugins[@]}" --save-dev --no-bin-links
+npm install gulp $@ --save-dev --no-bin-links
 
 echo ">>>> Done"
 
